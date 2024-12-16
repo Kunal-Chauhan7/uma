a306695eef32f4837d895e0cebd39cb516d09963
Kunal Chauhan <kunalchauhanlion@gmail.com>
2024-12-17 01:53:32 +0530

    done making database commands

--- database/handledata.mjs
+++ database/handledata.mjs
@@ -1,10 +1,16 @@
 // app.js
-import { getDb, Search , addUser } from './database.mjs';
+import { getDb, Search , addUser , isUserExist , updateUserName, updateUserWallet, updateUserRelationship, updateUserBio, updateUserGender} from './database.mjs';
 
 const main = async () => {
   // const user = await Search(917404799807);
+  // console.log(await isUserExist(917404799807));
   // console.log(user);
-  addUser({ id: 917404799807, name: 'Kunal Chauhan' });
+  // addUser({ id: 917404799807, name: 'Kunal Chauhan' });
+  // await updateUserName(917404799807 , 'Kunal singh Chauhan');
+  // await updateUserWallet(917404799807 , 100);
+  // await updateUserRelationship(917404799807 , 'married');
+  // await updateUserBio(917404799807 , 'I am a good person');
+  // await updateUserGender(917404799807,'pokemon');
 };
 
 main();
